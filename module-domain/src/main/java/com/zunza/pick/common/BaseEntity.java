33d455c45151925d1247aa276a91aba2b396935a
zunza <fksfks015@naver.com>
2024-10-24 23:19:56 +0900

    feat: BaseEntity 추가

---
+++ module-domain/src/main/java/com/zunza/pick/common/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.zunza.pick.common;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@CreatedDate
+	private Instant createdDt = Instant.now();
+
+	@LastModifiedDate
+	private Instant modifiedDt = Instant.now();
+}
