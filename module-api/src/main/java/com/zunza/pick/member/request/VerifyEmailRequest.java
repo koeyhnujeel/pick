d8a3bacd5bb8b65ff11eb083f54e4bf26a9548b1
zunza <fksfks015@naver.com>
2024-10-27 20:27:01 +0900

    feat: 이메일 중복 확인을 위한 request 클래스 추가

---
+++ module-api/src/main/java/com/zunza/pick/member/request/VerifyEmailRequest.java
@@ -0,0 +1,15 @@
+package com.zunza.pick.member.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class VerifyEmailRequest {
+
+	@Email(message = "유요한 이메일 형식이 아닙니다.")
+	@NotBlank(message = "이메일을 입력해 주세요.")
+	private String email;
+}
