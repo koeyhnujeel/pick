dfff52c914989f2217cf6b35f2591a82c2f1a04d
zunza <fksfks015@naver.com>
2024-10-27 20:25:35 +0900

    feat: 회원가입 기능 추가 (service)

---
+++ module-domain/src/main/java/com/zunza/pick/domain/member/service/AuthService.java
@@ -0,0 +1,29 @@
+package com.zunza.pick.domain.member.service;
+
+import org.springframework.stereotype.Service;
+
+import com.zunza.pick.domain.member.dto.SignupDto;
+import com.zunza.pick.domain.member.dto.VerifyEmailDto;
+import com.zunza.pick.domain.member.entity.Member;
+import com.zunza.pick.domain.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+	private final MemberRepository memberRepository;
+
+	public void signup(SignupDto signupDto) {
+		Member member = Member.from(signupDto);
+		memberRepository.save(member);
+	}
+
+	public void verifyDuplicateEmail(VerifyEmailDto verifyEmailDto) {
+		Boolean duplicateEmail = memberRepository.isDuplicateEmail(verifyEmailDto.getEmail());
+		if (duplicateEmail) {
+			throw new IllegalArgumentException("사용 중인 이메일 입니다.");
+		}
+	}
+}
