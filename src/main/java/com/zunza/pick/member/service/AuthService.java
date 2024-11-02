4597f350a4def10711036919eca8a9ab6950faf7
zunza <fksfks015@naver.com>
2024-11-03 00:23:52 +0900

    feat: 닉네임, 폰 중복 검사 추가 (Service)

--- src/main/java/com/zunza/pick/member/service/AuthService.java
+++ src/main/java/com/zunza/pick/member/service/AuthService.java
@@ -2,8 +2,12 @@ package com.zunza.pick.member.service;
 
 import org.springframework.stereotype.Service;
 
+import com.zunza.pick.exception.DuplicateNicknameException;
+import com.zunza.pick.exception.DuplicatePhoneException;
 import com.zunza.pick.member.dto.SignupDto;
 import com.zunza.pick.member.dto.VerifyEmailDto;
+import com.zunza.pick.member.dto.VerifyNicknameDto;
+import com.zunza.pick.member.dto.VerifyPhoneDto;
 import com.zunza.pick.member.entity.Member;
 import com.zunza.pick.member.repository.MemberRepository;
 import com.zunza.pick.exception.DuplicateEmailException;
@@ -25,6 +29,20 @@ public class AuthService {
 		Boolean duplicateEmail = memberRepository.isDuplicateEmail(verifyEmailDto.getEmail());
 		if (duplicateEmail) {
 			throw new DuplicateEmailException();
+		}
+	}
+
+	public void verifyDuplicateNickname(VerifyNicknameDto verifyNicknameDto) {
+		Boolean duplicateNickname = memberRepository.isDuplicateNickname(verifyNicknameDto.getNickname());
+		if (duplicateNickname) {
+			throw new DuplicateNicknameException();
+		}
+	}
+
+	public void verifyDuplicatePhone(VerifyPhoneDto verifyPhoneDto) {
+		Boolean duplicatePhone = memberRepository.isDuplicatePhone(verifyPhoneDto.getPhone());
+		if (duplicatePhone) {
+			throw new DuplicatePhoneException();
 		}
 	}
 }
