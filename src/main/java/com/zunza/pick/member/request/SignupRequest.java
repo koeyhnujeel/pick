f19a122c73fccd209f5517cf14bdbf79b13c1e44
zunza <fksfks015@naver.com>
2024-11-03 19:21:41 +0900

    feat: 중복 확인 여부 필드 추가

--- src/main/java/com/zunza/pick/member/request/SignupRequest.java
+++ src/main/java/com/zunza/pick/member/request/SignupRequest.java
@@ -2,6 +2,7 @@ package com.zunza.pick.member.request;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.zunza.pick.annotations.SpecificValue;
 import com.zunza.pick.member.constant.MemberType;
 
 import jakarta.validation.constraints.Email;
@@ -18,6 +19,9 @@ public class SignupRequest {
 	@NotBlank(message = "이메일을 입력해 주세요.")
 	private String email;
 
+	@SpecificValue(acceptedValues = "TRUE", message = "이메일 중복 확인을 진행해 주세요.")
+	private String isEmailVerified;
+
 	@NotBlank(message = "비밀번호를 입력해 주세요.")
 	@Length(min = 8, max = 16, message = "비밀번호는 8자~16자로 설정해 주세요.")
 	private String password;
@@ -30,11 +34,19 @@ public class SignupRequest {
 	@Length(min = 2, max = 10, message = "닉네임은 2자~10자로 설정해 주세요.")
 	private String nickname;
 
+	@SpecificValue(acceptedValues = "TRUE", message = "닉네임 중복 확인을 진행해 주세요.")
+	private String isNicknameVerified;
+
+
 	@NotBlank(message = "주소를 입력해 주세요.")
 	private String address;
 
 	@NotBlank(message = "핸드폰 번호를 입력해 주세요.")
 	private String phone;
+
+	@SpecificValue(acceptedValues = "TRUE", message = "번호 중복 확인을 진행해 주세요.")
+	private String isPhoneVerified;
+
 
 	@NotNull(message = "회원 유형을 선택해 주세요.")
 	private MemberType memberType;
