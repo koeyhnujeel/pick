b1e3c1bb4476729ce65956b7d3a5bee3f42e1b98
zunza <fksfks015@naver.com>
2024-11-03 19:21:59 +0900

    feat: 중복 확인 커스텀 어노테이션 추가

---
+++ src/main/java/com/zunza/pick/annotations/SpecificValue.java
@@ -0,0 +1,24 @@
+package com.zunza.pick.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.zunza.pick.commons.SpecificValueValidator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+@Documented
+@Constraint(validatedBy = SpecificValueValidator.class)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SpecificValue {
+	String message() default "Invalid value";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+
+	String[] acceptedValues(); // 허용되는 값 목록
+}
