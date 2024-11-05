59b0fd4a24d481105af1c586ca0533fbeda5b6ac
zunza <fksfks015@naver.com>
2024-11-05 16:07:12 +0900

    feat: security 설정 추가

---
+++ src/main/java/com/zunza/pick/config/SecurityConfig.java
@@ -0,0 +1,74 @@
+package com.zunza.pick.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zunza.pick.jwt.JwtLoginFilter;
+import com.zunza.pick.jwt.JwtTokenProvider;
+import com.zunza.pick.member.repository.TokenRedisRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final AuthenticationConfiguration authenticationConfiguration;
+	private final TokenRedisRepository tokenRedisRepository;
+	private final JwtTokenProvider jwtTokenProvider;
+	private final ObjectMapper objectMapper;
+
+	@Bean
+	public JwtLoginFilter jwtLoginFilter() throws Exception {
+		JwtLoginFilter filter = new JwtLoginFilter(
+			getAuthenticationManager(authenticationConfiguration),
+			tokenRedisRepository,
+			jwtTokenProvider,
+			objectMapper
+		);
+		filter.setAuthenticationManager(getAuthenticationManager(authenticationConfiguration));
+		filter.setFilterProcessesUrl("/api/auth/login");
+		return filter;
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+			.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers("/login").permitAll()
+				.requestMatchers("/api/auth/signup").permitAll()
+			)
+
+			.addFilterAt(jwtLoginFilter(),
+				UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager getAuthenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+}
