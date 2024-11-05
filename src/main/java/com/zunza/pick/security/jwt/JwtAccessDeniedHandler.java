68cf9117c2a32dc888d4e5e078901837a81fd96b
zunza <fksfks015@naver.com>
2024-11-06 01:33:41 +0900

    feat: 인가에 대한 예외 핸들러 추가

---
+++ src/main/java/com/zunza/pick/security/jwt/JwtAccessDeniedHandler.java
@@ -0,0 +1,39 @@
+package com.zunza.pick.security.jwt;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zunza.pick.commons.ErrorResponse;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+	private final ObjectMapper objectMapper;
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+		AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+		ErrorResponse errorResponse = ErrorResponse.builder()
+			.message("접근 권한이 없습니다.")
+			.code(HttpServletResponse.SC_FORBIDDEN)
+			.build();
+
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+		objectMapper.writeValue(response.getWriter(), errorResponse);
+	}
+}
