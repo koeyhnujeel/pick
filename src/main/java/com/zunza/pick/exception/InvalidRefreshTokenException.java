4f5247caa787f249fe2e76abc81549904a7f0586
zunza <fksfks015@naver.com>
2024-11-06 17:05:39 +0900

    feat: refreshToken 예외 추가

---
+++ src/main/java/com/zunza/pick/exception/InvalidRefreshTokenException.java
@@ -0,0 +1,17 @@
+package com.zunza.pick.exception;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public class InvalidRefreshTokenException extends CustomException {
+
+	private static final String MESSAGE = "유효하지 않은 Refresh 토큰입니다.";
+
+	public InvalidRefreshTokenException() {
+		super(MESSAGE);
+	}
+
+	@Override
+	public int getStatusCode() {
+		return HttpServletResponse.SC_UNAUTHORIZED;
+	}
+}
