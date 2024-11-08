495be9c85048aa608c008f7d9e761162bdc4fd7d
zunza <fksfks015@naver.com>
2024-11-08 18:38:16 +0900

    feat: Object Storage에 이미지 업로드 기능 추가(Controller)

---
+++ src/main/java/com/zunza/pick/external/objectStorage/controller/ObjectStorageController.java
@@ -0,0 +1,29 @@
+package com.zunza.pick.external.objectStorage.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.zunza.pick.external.objectStorage.controller.response.UploadImageResponse;
+import com.zunza.pick.external.objectStorage.service.ObjectStorageService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class ObjectStorageController {
+
+	private final ObjectStorageService objectStorageService;
+
+	@PostMapping("/api/seller/products/images")
+	public ResponseEntity<UploadImageResponse> uploadImage(@RequestPart List<MultipartFile> files) throws
+		IOException {
+		UploadImageResponse uploadImageResponses = objectStorageService.uploadFile(files);
+		return ResponseEntity.ok(uploadImageResponses);
+	}
+}
