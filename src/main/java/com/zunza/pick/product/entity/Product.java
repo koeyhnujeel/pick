b93817b54735a8810ad2d2641d3a8a874ba7d0aa
zunza <fksfks015@naver.com>
2024-11-08 18:39:19 +0900

    refactor: 이미지 엔티티로 분리

--- src/main/java/com/zunza/pick/product/entity/Product.java
+++ src/main/java/com/zunza/pick/product/entity/Product.java
@@ -1,12 +1,14 @@
 package com.zunza.pick.product.entity;
 
 import com.zunza.pick.commons.BaseEntity;
+import com.zunza.pick.product.controller.dto.AddProductDto;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -28,19 +30,31 @@ public class Product extends BaseEntity {
 	private int stock;
 
 	@Column(nullable = false)
-	private String mainImage;
-
-	@Column(nullable = false)
-	private String mainThumbnail;
-
-	@Column(nullable = false)
-	private String detailImage;
-
-	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private Category category;
 
 	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private ProductStatus status;
+
+	@Builder
+	private Product(String name, int price, String description, int stock, Category category, ProductStatus status) {
+		this.name = name;
+		this.price = price;
+		this.description = description;
+		this.stock = stock;
+		this.category = category;
+		this.status = status;
+	}
+
+	public static Product from(AddProductDto addProductDto) {
+		return Product.builder()
+			.name(addProductDto.getName())
+			.price(addProductDto.getPrice())
+			.description(addProductDto.getDescription())
+			.stock(addProductDto.getStock())
+			.category(addProductDto.getCategory())
+			.status(addProductDto.getStatus())
+			.build();
+	}
 }
