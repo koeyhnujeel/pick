df7f9a63ebdc94cda1e3967e6f66fdff42612b6a
zunza <fksfks015@naver.com>
2024-11-08 18:41:49 +0900

    feat: 상품 등록 기능 추가(Controller)

---
+++ src/main/java/com/zunza/pick/product/controller/seller/ProductController.java
@@ -0,0 +1,29 @@
+package com.zunza.pick.product.controller.seller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zunza.pick.product.controller.dto.AddProductDto;
+import com.zunza.pick.product.controller.request.AddProductRequest;
+import com.zunza.pick.product.controller.response.AddProductResponse;
+import com.zunza.pick.product.service.ProductService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductController {
+
+	private final ProductService productService;
+
+	@PostMapping("/api/seller/products")
+	public ResponseEntity<AddProductResponse> addProduct(@Valid @RequestBody AddProductRequest addProductRequest) {
+		AddProductDto addProductDto = AddProductDto.from(addProductRequest);
+		AddProductResponse addProductResponse = productService.addProduct(addProductDto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(addProductResponse);
+	}
+}
