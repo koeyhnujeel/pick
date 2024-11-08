2be23bca732e92c3e3f278096999de75555972f9
zunza <fksfks015@naver.com>
2024-11-08 18:35:56 +0900

    Data Init

--- src/main/java/com/zunza/pick/DataInit.java
+++ src/main/java/com/zunza/pick/DataInit.java
@@ -1,10 +1,13 @@
 package com.zunza.pick;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import com.zunza.pick.member.constant.MemberType;
+import com.zunza.pick.member.entity.MemberType;
 import com.zunza.pick.member.entity.Member;
 import com.zunza.pick.member.repository.MemberRepository;
 
@@ -19,16 +22,42 @@ public class DataInit implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Member member = Member.builder()
-			.email("test@email.com")
+		List<Member> members = new ArrayList<>();
+
+		Member customer = Member.builder()
+			.email("customer@email.com")
 			.password(passwordEncoder.encode("password"))
-			.name("홍길동")
-			.nickname("dong")
-			.address("조선 한양")
+			.name("김고객")
+			.nickname("best customer")
+			.address("서울 강남")
 			.phone("010-1234-5678")
 			.memberType(MemberType.CUSTOMER)
 			.build();
 
-		memberRepository.save(member);
+		Member seller = Member.builder()
+			.email("seller@email.com")
+			.password(passwordEncoder.encode("password"))
+			.name("김판매")
+			.nickname("best seller")
+			.address("서울 강남")
+			.phone("010-1234-5671")
+			.memberType(MemberType.SELLER)
+			.build();
+
+		Member admin = Member.builder()
+			.email("admin@email.com")
+			.password(passwordEncoder.encode("password"))
+			.name("김관리")
+			.nickname("best admin")
+			.address("서울 강남")
+			.phone("010-1232-5671")
+			.memberType(MemberType.ADMIN)
+			.build();
+
+		members.add(customer);
+		members.add(seller);
+		members.add(admin);
+
+		memberRepository.saveAll(members);
 	}
 }
