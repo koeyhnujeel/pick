58e18cde8748b8aedff8d7db906aa48bea96c4d0
zunza <fksfks015@naver.com>
2024-11-10 19:07:14 +0900

    Data Init

--- src/main/java/com/zunza/pick/DataInit.java
+++ src/main/java/com/zunza/pick/DataInit.java
@@ -3,6 +3,7 @@ package com.zunza.pick;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,12 @@ import org.springframework.stereotype.Component;
 import com.zunza.pick.member.entity.MemberType;
 import com.zunza.pick.member.entity.Member;
 import com.zunza.pick.member.repository.MemberRepository;
+import com.zunza.pick.product.entity.Category;
+import com.zunza.pick.product.entity.Product;
+import com.zunza.pick.product.entity.ProductImage;
+import com.zunza.pick.product.entity.ProductStatus;
+import com.zunza.pick.product.repository.ProductImageRepository;
+import com.zunza.pick.product.repository.ProductRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,6 +26,14 @@ public class DataInit implements CommandLineRunner {
 
 	private final MemberRepository memberRepository;
 	private final PasswordEncoder passwordEncoder;
+	private final ProductRepository productRepository;
+	private final ProductImageRepository productImageRepository;
+
+	@Value("${test.mainImageUrl}")
+	private String mainImageUrl;
+
+	@Value("${test.detailImageUrl}")
+	private String detailImageUrl;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -59,5 +74,39 @@ public class DataInit implements CommandLineRunner {
 		members.add(admin);
 
 		memberRepository.saveAll(members);
+
+		for (int i = 1; i < 101; i++) {
+			Product product = Product.builder()
+				.name("옷" + i)
+				.price(20000 + i * 100)
+				.description("옷 입니다~" + i)
+				.stock(i)
+				.category(Category.TOP)
+				.status(ProductStatus.IN_STOCK)
+				.build();
+			Product saved = productRepository.save(product);
+
+			ProductImage mainImage = ProductImage.of(saved, mainImageUrl);
+			ProductImage detailImage = ProductImage.of(saved, detailImageUrl);
+			productImageRepository.save(mainImage);
+			productImageRepository.save(detailImage);
+		}
+
+		for (int i = 1; i < 101; i++) {
+			Product product = Product.builder()
+				.name("옷" + (i + 100))
+				.price(20000 + i * 100)
+				.description("옷 입니다~" + (i + 100))
+				.stock(i)
+				.category(Category.TOP)
+				.status(ProductStatus.IN_STOCK)
+				.build();
+			Product saved = productRepository.save(product);
+
+			ProductImage mainImage = ProductImage.of(saved, mainImageUrl);
+			ProductImage detailImage = ProductImage.of(saved, detailImageUrl);
+			productImageRepository.save(mainImage);
+			productImageRepository.save(detailImage);
+		}
 	}
 }
