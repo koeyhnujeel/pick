9ee3e89b07420f610fddafe9a37edd5815214b5e
zunza <fksfks015@naver.com>
2024-11-20 02:41:28 +0900

    feat: 멤버 쿠폰 저장하는 여러 방식을 구현해보고자 만든 인터페이스

---
+++ src/main/java/com/zunza/pick/coupon/strategy/MemberCouponCreationStrategy.java
@@ -0,0 +1,7 @@
+package com.zunza.pick.coupon.strategy;
+
+import com.zunza.pick.coupon.entity.MemberCoupon;
+
+public interface MemberCouponCreationStrategy {
+	void create(MemberCoupon memberCoupon);
+}
